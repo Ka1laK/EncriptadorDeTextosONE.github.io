ee2f0135e948a1c0697ab1c1b20aea86cf4072dd
Ka1laK <rissel1771@gmail.com>
2024-07-25 17:31:12 -0500

    encriptador_ONE

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const input = document.querySelector("#input");
 const copy = document.getElementById("copiar");
 
-document.querySelector("#encriptar").addEventListener("click", () => { encriptar("encriptar") });
-document.querySelector("#desencriptar").addEventListener("click", () => { encriptar("desencriptar") });
-document.querySelector("#copiar").addEventListener("click",()=>{copiarEncriptado()});
+document.querySelector("#encriptar").addEventListener("click", () => {
+    if (input.value.trim() === '') {
+        alert('Por favor, ingrese texto en el campo');
+    } else {
+        encriptar("encriptar");
+    }
+});
+
+document.querySelector("#desencriptar").addEventListener("click", () => {
+    if (input.value.trim() === '') {
+        alert('Por favor, ingrese texto en el campo');
+    } else {
+        encriptar("desencriptar");
+    }
+});
+
+document.querySelector("#copiar").addEventListener("click", () => { copiarEncriptado() });
 
 function Limpiando_Input() {
     formatearOutput();
@@ -15,52 +29,47 @@ function Limpiando_Input() {
     inputLimpio = inputLimpio.replaceAll('ú', 'u');
     inputLimpio = inputLimpio.replaceAll('ñ', 'n');
 
-    return inputLimpio
-
+    return inputLimpio;
 }
 
 function formatearOutput() {
+    const output = document.getElementById("output");
+    const outputContainer = document.querySelector(".output");
     if (input.value === "") {
-
-        document.getElementById("output").setAttribute("class", "output-image");
-        document.getElementById("output").value = "";
+        output.setAttribute("class", "output-empty");
+        output.value = "";
         document.querySelectorAll(".output>h3, .output>p").forEach(el => el.removeAttribute("hidden"));
-        document.getElementById("copiar").setAttribute("hidden", "")
-
-    }
-    else {
-        document.getElementById("output").removeAttribute("class");
+        document.getElementById("copiar").setAttribute("hidden", "");
+    } else {
+        output.removeAttribute("class");
         document.querySelectorAll(".output>h3, .output>p").forEach(el => el.setAttribute('hidden', ''));
-        document.getElementById("copiar").removeAttribute("hidden")
+        document.getElementById("copiar").removeAttribute("hidden");
     }
 }
 
 function encriptar(string) {
-
-    const inputLimpio = Limpiando_Input()
+    const inputLimpio = Limpiando_Input();
     const cambio = {
         a: "ai",
         e: "enter",
         i: "imes",
         o: "ober",
         u: "ufat"
-    }
+    };
 
     if (string === "encriptar") {
-        let arreglo_string = []
-
+        let arreglo_string = [];
         for (let i = 0; i < inputLimpio.length; i++) {
             if (cambio.hasOwnProperty(inputLimpio[i])) {
                 arreglo_string.push(cambio[inputLimpio[i]]);
-
-            } else arreglo_string.push(inputLimpio[i]);
+            } else {
+                arreglo_string.push(inputLimpio[i]);
+            }
         }
-        document.getElementById("output").value = arreglo_string.join('')
-
+        document.getElementById("output").value = arreglo_string.join('');
     } else if (string === "desencriptar") {
         const desencriptar = Object.values(cambio);
         let palabra_nueva = inputLimpio;
-
         for (let i = 0; i < desencriptar.length; i++) {
             palabra_nueva = palabra_nueva.replaceAll(desencriptar[i], desencriptar[i][0]);
         }
@@ -68,12 +77,9 @@ function encriptar(string) {
     }
 }
 
-function copiarEncriptado(){
-
+function copiarEncriptado() {
     let copy = document.querySelector("#output").value;
-
-    navigator.clipboard.writeText(copy).then(()=>{
-
+    navigator.clipboard.writeText(copy).then(() => {
         document.getElementById("input").value = copy;
         document.getElementById("output").value = "";
     });
@@ -83,5 +89,6 @@ const limpiarBoton = document.querySelector('#limpiar');
 const entradaTexto = document.querySelector('#input');
 
 limpiarBoton.addEventListener('click', () => {
-  entradaTexto.value = '';
+    entradaTexto.value = '';
+    formatearOutput();
 });
